221f6602fd8f30cd4ff986c3904398bf4fb11a79
Chandrasekaran Janardhanan <jjchandra@gmail.com>
2020-12-27 08:43:12 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toFahrenheit(celsius) {
+    //var celsius = 12;
+    console.log('Celsius: ' + celsius);
+    var fahrenheit = (celsius * 9 / 5) + 32;
+    console.log('Fahrenheit: ' + fahrenheit);
+    return fahrenheit;
+}
